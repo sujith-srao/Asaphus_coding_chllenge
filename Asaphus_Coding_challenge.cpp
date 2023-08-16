17e781376b131656bd99c3afb9251393bec75695
Sujith Rao <sujith.srao@gmail.com>
2023-08-16 19:13:18 +0200

    Initial working code

--- Asaphus_Coding_challenge.cpp
+++ Asaphus_Coding_challenge.cpp
@@ -9,82 +9,60 @@
 #include <memory>
 #include <numeric>
 #include <vector>
-
+#include "Box.h"
+#include "Player.h"
 using namespace std;
 
 
+std::pair<double, double> play(const std::vector<uint32_t>& input_weights) {
+    std::vector<std::unique_ptr<Box> > boxes;
+    boxes.emplace_back(Box::makeGreenBox(0.0));
+    boxes.emplace_back(Box::makeGreenBox(0.1));
+    boxes.emplace_back(Box::makeBlueBox(0.2));
+    boxes.emplace_back(Box::makeBlueBox(0.3));
 
+    Player player_A;
+    Player player_B;
 
-class BlueBox;
-class GreenBox;
+    for (int i = 0; i < input_weights.size(); i++) {
+        
+        if (i % 2 == 0) {
+            player_A.takeTurn(input_weights[i], boxes);
+        }
+        else {
+            player_B.takeTurn(input_weights[i], boxes);
+        }
+    }
 
-class Box {
-public:
-    explicit Box(double i);
-    
-    static std::unique_ptr<Box> makeGreenBox(double initial_weight);
-    static std::unique_ptr<Box> makeBlueBox(double initial_weight);
-    
-    bool operator<(const Box& rhs) const;
-    
-    virtual void absorbToken(double token_weight)=0;
-    virtual double outputScore() = 0;
-
-protected:
-    double weight_;
-};
-
-
-class GreenBox :public Box {
-    using Box::Box;
-public:
-    void absorbToken(double token_weight) override;
-    double outputScore() override;
-};
-
-class BlueBox :public Box {
-    using Box::Box;
-public:
-    void absorbToken(double token_weight) override;
-    double outputScore() override;
-};
-
-Box::Box(double initial_weight):weight_(initial_weight) {
-
-}
-
-std::unique_ptr<Box> Box::makeGreenBox(double initial_weight) {
-    std::unique_ptr<Box> greenbox = std::make_unique<GreenBox>(initial_weight);
-    return greenbox;
-}
-
-std::unique_ptr<Box> Box::makeBlueBox(double initial_weight) {
-    std::unique_ptr<Box> bluebox = std::make_unique<BlueBox>(initial_weight);
-    return bluebox;
-}
-
-bool Box::operator<(const Box& rhs) const {
-    return weight_ < rhs.weight_;
-}
-
-void GreenBox::absorbToken(double token_weight) {
-
-}
-
-void BlueBox::absorbToken(double token_weight) {
-
-}
-
-double GreenBox::outputScore() {
-    return 0;
-}
-
-double BlueBox::outputScore() {
-    return 0;
+    std::cout << "Scores: player A " << player_A.getScore() << ", player B "
+        << player_B.getScore() << std::endl;
+    return std::make_pair(player_A.getScore(), player_B.getScore());
 }
 
 int main()
 {
+    std::vector<uint32_t> inputs{ 1, 1, 2, 3 };
+    auto result = play(inputs);
+    return 0;
+
+    double initial_weight = 0.0;
+    std::unique_ptr<Box> greenBox = Box::makeGreenBox(initial_weight);
+    
+    greenBox->absorbToken(1);
+    cout << "Output Score after pushing 1 " << greenBox->outputScore() << endl;;
+
+    greenBox->absorbToken(5);
+    cout<<"Output Score after pushing 5 "<<greenBox->outputScore() << endl;;
+    
+    initial_weight = 0.2;
+    std::unique_ptr<Box> blueBox = Box::makeBlueBox(initial_weight);
+
+    blueBox->absorbToken(2);
+    cout << "Output Score after pushing 2 " << blueBox->outputScore() << endl;;
+
+    blueBox->absorbToken(13);
+    cout << "Output Score after pushing 13 " << blueBox->outputScore() << endl;;
+
     std::cout << "Hello World!\n";
 }
 
